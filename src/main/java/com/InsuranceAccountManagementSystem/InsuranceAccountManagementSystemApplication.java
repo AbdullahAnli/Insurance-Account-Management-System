f62df5117cc6e603ab2194ff15304247d8c3f776
Abdullah Anli <anliabdullah08@gmail.com>
2024-05-17 16:41:30 +0300

    project requirements were determined, pom.xml file was created

---
+++ src/main/java/com/InsuranceAccountManagementSystem/InsuranceAccountManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.InsuranceAccountManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceAccountManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceAccountManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InsuranceAccountManagementSystem/InsuranceAccountManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InsuranceAccountManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceAccountManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
